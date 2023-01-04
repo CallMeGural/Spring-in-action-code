62d363de8551d5aee4ec5e8096db6a8aec9c7167
CallMeGural <76165491+CallMeGural@users.noreply.github.com>
2023-01-04 11:51:12 +0100

    Add files via upload

---
+++ taco-cloud/src/main/java/pl/tacocloud/tacos/Order.java
@@ -0,0 +1,30 @@
+package pl.tacocloud.tacos;
+
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+
+@Data
+public class Order {
+    @NotEmpty(message = "Musisz podać imię i nazwisko")
+    private String name;
+    @NotEmpty(message = "Musisz podać nazwę ulicy oraz numer domu")
+    private String street;
+    @NotEmpty(message = "Musisz podać nazwę miejscowości")
+    private String city;
+    @NotEmpty(message = "Musisz podać nazwę województwa, w którym znajduje się podana miejscowość")
+    private String state;
+    @NotEmpty(message = "Musisz podać kod pocztowy miejscowości")
+    private String zip;
+    @CreditCardNumber(message = "To nie jest prawidłowy numer karty kredytowej")
+    private String ccNumber;
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([0-9][0-9])$",
+            message = "Wartość musi być w formacie MM/RR")
+    private String ccExpiration;
+    @Digits(integer = 3, fraction = 0, message = "Nieprawidłowy kod CVV")
+    private String ccCVV;
+
+}
---
+++ taco-cloud/src/main/java/pl/tacocloud/tacos/web/DesignTacoController.java
@@ -0,0 +1,70 @@
+package pl.tacocloud.tacos.web;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.tacocloud.tacos.Ingredient;
+import pl.tacocloud.tacos.Ingredient.Type;
+import pl.tacocloud.tacos.Taco;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+         new Ingredient("FLTO","pszenna", Type.WRAP),
+         new Ingredient("COTO","kukurydziana", Type.WRAP),
+         new Ingredient("GRBF","mielona wołowina", Type.PROTEIN),
+         new Ingredient("CARN","kawałki mięsa", Type.PROTEIN),
+         new Ingredient("TMTO","pomidory w kostke", Type.VEGGIES),
+         new Ingredient("LETC","sałata", Type.VEGGIES),
+         new Ingredient("CHED","cheddar", Type.CHEESE),
+         new Ingredient("JACK","Monterey Jack", Type.CHEESE),
+         new Ingredient("SLSA","pikantny sos pomidorowy", Type.SAUCE),
+         new Ingredient("SRCR","śmietana", Type.SAUCE)
+        );
+
+        Type[] types = Ingredient.Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
+        }
+    }
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        model.addAttribute("design",new Taco()); //kopiowanie atrybutów z modelu do serwletu odpowiedzi
+        return "design";
+    }
+    @PostMapping
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "redirect:/design";
+        }
+
+        // this will save our Taco Design
+         log.info("Przetwarzanie projektu: " + design);
+
+        return "redirect:/orders/current";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients.stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+}
